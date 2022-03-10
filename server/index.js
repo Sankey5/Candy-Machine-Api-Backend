f96cee8315c9cdaeaaba0bf9945a69b0e38c8c18
Brian Sankey <briansankey93@gmail.com>
2022-03-10 15:57:11 -0600

    Added second api endpoint to upload files to

--- server/index.js
+++ server/index.js
@@ -14,11 +14,15 @@ app.use(cors());
 app.post("/cm", (req, res) => {
   console.log(req.body);
   // Validate the candy machine configuration
-  const config = validate(req.body);
+  //const config = validate(req.body);
 
   // Execute the candy machine to be created
   // Send back a candy machine ID if the execution has started correctly
   res.send(req.body);
+});
+
+app.post("/files", (req, res) => {
+  console.log(req.body);
 });
 
 // Get candy machine status of all currently creating candy machines
